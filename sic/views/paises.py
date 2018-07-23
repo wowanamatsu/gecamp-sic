cf013ace6e1e91a063225f59732a5ae4e38e9ad3
wowanamatsu <antonioflavionte@gmail.com>
2018-07-23 13:56:05 -0300

    Implementação do sistema de paginação.

--- sic/views/paises.py
+++ sic/views/paises.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from ..models import Pais
 
 def paises(request):
     paises = Pais.objects.all()
+
+    paginator = Paginator(paises, 10)
+
+    page = request.GET.get('page')
+
+    paises = paginator.get_page(page)
+
     return render(request, 'paises/index.html', {'paises':paises})
